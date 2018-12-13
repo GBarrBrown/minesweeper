a81c9fc0f14776268f4ce62aa23c8832113c2209
George-BarrBrown <gbb7899@gmail.com>
2018-12-13 13:43:54 +1300

    checkForWin() working

--- minesweeper.js
+++ minesweeper.js
@@ -11,9 +11,14 @@ var board = {
 };
 
 function startGame () {
-  for (var i=0; i < board.cells.length; i++) {
+  // spent ages with ("click", checkForWin()); and was confused as to why it ran the function immediately
+  document.addEventListener("click", checkForWin);
+  document.addEventListener("contextmenu", checkForWin);
+
+  for (var i = 0; i < board.cells.length; i++) {
     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
   }
+
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
 }
@@ -23,6 +28,28 @@ function startGame () {
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
 function checkForWin () {
+
+
+  var mineCount = 0;
+  // used to hold num of mines that are currently flagged
+  var markedMineCount = 0;
+
+  var nonMineHiddenCount = 0;
+  // console.log("Testing");
+  for (var j = 0; j < board.cells.length; j++) {
+    // console.log(board.cells[j]);
+    if (board.cells[j].isMine == true && board.cells[j].isMarked == true) {
+      mineCount++;
+      markedMineCount++;
+    } else if (board.cells[j].isMine){
+      mineCount++;
+    } else if (board.cells[j].hidden == true) {
+      nonMineHiddenCount++;
+    }
+  }
+  if (mineCount == markedMineCount && nonMineHiddenCount == 0) {
+    lib.displayMessage("You Win!")
+  }
 
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
